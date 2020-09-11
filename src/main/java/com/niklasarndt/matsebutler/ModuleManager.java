2de10f547d35a9200914502de3c98d58c1d0b3ce
Niklas Arndt <niklasarndt28@gmail.com>
2020-09-11 15:16:24 +0200

    Code optimizations
    Signed-off-by: Niklas Arndt <niklasarndt28@gmail.com>

--- src/main/java/com/niklasarndt/matsebutler/ModuleManager.java
+++ src/main/java/com/niklasarndt/matsebutler/ModuleManager.java
@@ -9,7 +9,12 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 
 /**
